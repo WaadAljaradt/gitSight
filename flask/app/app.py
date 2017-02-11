324cd5ecbabd2fd901238a3a681bcbb88204e359
Ubuntu <ubuntu@ip-172-31-2-39.us-west-2.compute.internal>
2017-02-11 03:05:09 +0000

    add app file to land to home page index.html

---
+++ flask/app/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, flash, render_template, jsonify,request
+import sys
+import json
+from controler import *
+import redis
+import imp
+
+
+RedisConfig = imp.load_source('RedisConfig', 'redis_conf/RedisConfig.py')
+
+from RedisConfig import RedisConfig
+
+cfg = RedisConfig()
+app = Flask(__name__)
+app.secret_key = 'some_secret'
+
+@app.route('/')
+def index():
+        words = getWords()
+        return render_template("index.html",value ="",words=words)
