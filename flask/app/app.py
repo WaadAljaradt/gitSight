fc3cc3049e0516d58c7c32304d02299658c12d94
Ubuntu <ubuntu@ip-172-31-2-39.us-west-2.compute.internal>
2017-02-11 21:15:12 +0000

    add search function to app.py to call retrieve from controller

--- flask/app/app.py
+++ flask/app/app.py
@@ -18,3 +18,26 @@ app.secret_key = 'some_secret'
 def index():
         words = getWords()
         return render_template("bubble.html",value ="",words=words)
+
+
+
+@app.route('/search',methods=['POST'])
+def search():
+        str = request.form.get('search')
+        stars = request.form.getlist('sort_by')
+        if(str.contains(""))
+        data =[]
+        if stars:
+                data= retrieve(str,2)
+        else :
+                data= retrieve(str,1)
+        flag = 0
+        if(not data):
+                docs =[]
+                tags=[]
+                flash(u'No results were found, please try again!', 'error')
+        else:
+                docs=data[0]
+                tags=data[1]
+
+        return render_template("index.html",hits = docs ,tags=tags,value = str)
